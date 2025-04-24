407382092eecf1d92671091237a7247c1f61e958
WiIIson <69092314+WiIIson@users.noreply.github.com>
2025-04-24 10:01:54 -0400

    Update index.js
    Added javascript to add links to mindmap

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,33 @@
 import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.mount(() => {
+      document.addEventListener("DOMContentLoaded", function () {
+        setTimeout(() => {
+          document.querySelectorAll("svg g.node").forEach((node) => {
+            const textElement = node.querySelector("text");
+            if (textElement) {
+              const nodeName = textElement.textContent.trim();
+              const links = {
+                "EF": "https://wiiison.github.io/EMGFlow-Python-Package/reference/feature-extraction.html",
+                "FA": "https://wiiison.github.io/EMGFlow-Python-Package/reference/file-access.html",
+                "DO": "https://wiiison.github.io/EMGFlow-Python-Package/reference/outlier-detection.html",
+                "PlS": "https://wiiison.github.io/EMGFlow-Python-Package/reference/plot-signals.html",
+                "PrS": "https://wiiison.github.io/EMGFlow-Python-Package/reference/preprocess-signals.html",
+              };
+              if (links[nodeName]) {
+                node.style.cursor = "pointer";
+                node.addEventListener("click", () => {
+                  window.location.href = links[nodeName];
+                });
+              }
+            }
+          });
+        }, 500);
+      });
+    });
+  }
+}
