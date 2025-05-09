bb524edec57d4ebf7509671535a458cd3dc05822
WiIIson <william@cconley.ca>
2025-05-09 11:04:24 -0400

    Added sample data, updated access_files tests
    Added sample_data_03.csv and sample_data_04.csv
    
    Renamed all test files.
    
    Updated access_files tests for new conventions.

--- EMGFlow-Package/src/EMGFlow/access_files.py
+++ EMGFlow-Package/src/EMGFlow/access_files.py
@@ -15,6 +15,112 @@ A collection of functions for accessing files.
 # =============================================================================
 #
 
+def make_paths(root=None):
+    """
+    Generates a file structure for signal files, and returns a dictionary of
+    the locations for these files.
+    
+    A 'Data' folder is created, with 'Raw', 'Notch', 'Bandpass', 'Smooth' and
+    'Feature' subfolders.
+
+    Parameters
+    ----------
+    root : str, optional
+        Root of the data to be generated. The default is None.
+
+    Returns
+    -------
+    pathNames : dict-str
+        A dictionary of file locations with keys for stage in the processing
+        pipeline.
+
+    """
+    
+    if root is None:
+        root = os.path.join(os.getcwd(), 'Data')
+    else:
+        root = os.path.normpath(root)
+    
+    # Create dictionary
+    pathNames = {
+        'Raw':os.path.join(root, 'Raw'),
+        'Notch':os.path.join(root, 'Notch'),
+        'Bandpass':os.path.join(root, 'Bandpass'),
+        'Smooth':os.path.join(root, 'Smooth'),
+        'Feature':os.path.join(root, 'Feature')
+    }
+    
+    # Create folders
+    for value in pathNames.values():
+        os.makedirs(value, exist_ok=True)
+    
+    # Return dictionary
+    return pathNames
+
+#
+# =============================================================================
+#
+
+def make_sample_data(pathNames):
+    """
+    Generates sample data in the 'Raw' folder of a provided dictionary of file
+    locations.
+
+    Parameters
+    ----------
+    pathNames : dict-str
+        Dictionary of file locations.
+
+    Raises
+    ------
+    Exception
+        An exception is raised if the provided 'pathNames' dictionary doesn't
+        contain a 'Raw' path key.
+    Exception
+        An exception is raised if the sample data cannot be loaded.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    # Check that a 'raw' folder exists
+    if 'Raw' not in pathNames:
+        raise Exception('Raw path not detected in pathNames.')
+    
+    # Load the sample data
+    try:
+        sample_data_01 = pd.read_csv(importlib_resources.files("EMGFlow").joinpath(os.path.join("data", "sample_data_01.csv")))
+        sample_data_02 = pd.read_csv(importlib_resources.files("EMGFlow").joinpath(os.path.join("data", "sample_data_02.csv")))
+        sample_data_03 = pd.read_csv(importlib_resources.files("EMGFlow").joinpath(os.path.join("data", "sample_data_03.csv")))
+        sample_data_04 = pd.read_csv(importlib_resources.files("EMGFlow").joinpath(os.path.join("data", "sample_data_04.csv")))
+    except:
+        raise Exception('Failed to load EMGFlow sample data.')
+    
+    # Write the sample data
+    os.makedirs(os.path.join(pathNames['Raw'], '01'), exist_ok=True)
+    os.makedirs(os.path.join(pathNames['Raw'], '02'), exist_ok=True)
+    
+    data_path_01 = os.path.join(pathNames['Raw'], '01', 'sample_data_01.csv')
+    data_path_02 = os.path.join(pathNames['Raw'], '01', 'sample_data_02.csv')
+    data_path_03 = os.path.join(pathNames['Raw'], '02', 'sample_data_03.csv')
+    data_path_04 = os.path.join(pathNames['Raw'], '02', 'sample_data_04.csv')
+    
+    if not os.path.exists(data_path_01):
+        sample_data_01.to_csv(data_path_01, index=False)
+    if not os.path.exists(data_path_02):
+        sample_data_02.to_csv(data_path_02, index=False)
+    if not os.path.exists(data_path_03):
+        sample_data_03.to_csv(data_path_03, index=False)
+    if not os.path.exists(data_path_04):
+        sample_data_04.to_csv(data_path_04, index=False)
+        
+
+#
+# =============================================================================
+#
+
 def read_file_type(path, fileExt):
     """
     Safe wrapper for reading files of a given extension.
@@ -209,7 +315,7 @@ def map_files_fuse(filedirs, names):
 
     Returns
     -------
-    filedirs : pd.DataFrame
+    pathDF : pd.DataFrame
         A dataframe of file names, and their locations in each file directory.
     
     """
@@ -228,103 +334,7 @@ def map_files_fuse(filedirs, names):
         # Add row to data frame
         data.append(row)
     # Create data frame
-    df = pd.DataFrame(data, columns=['ID', 'File'] + names)
-    df.set_index('ID',inplace=True)
+    pathDF = pd.DataFrame(data, columns=['ID', 'File'] + names)
+    pathDF.set_index('ID',inplace=True)
     
-    return df
-
-#
-# =============================================================================
-#
-
-def make_paths(root=None):
-    """
-    Generates a file structure for signal files, and returns a dictionary of
-    the locations for these files.
-    
-    A 'Data' folder is created, with 'Raw', 'Notch', 'Bandpass', 'Smooth' and
-    'Feature' subfolders.
-
-    Parameters
-    ----------
-    root : str, optional
-        Root of the data to be generated. The default is None.
-
-    Returns
-    -------
-    pathNames : dict-str
-        A dictionary of file locations with keys for stage in the processing
-        pipeline.
-
-    """
-    
-    if root is None:
-        root = os.path.join(os.getcwd(), 'Data')
-    else:
-        root = os.path.normpath(root)
-    
-    # Create dictionary
-    pathNames = {
-        'Raw':os.path.join(root, 'Raw'),
-        'Notch':os.path.join(root, 'Notch'),
-        'Bandpass':os.path.join(root, 'Bandpass'),
-        'Smooth':os.path.join(root, 'Smooth'),
-        'Feature':os.path.join(root, 'Feature')
-    }
-    
-    # Create folders
-    for value in pathNames.values():
-        os.makedirs(value, exist_ok=True)
-    
-    # Return dictionary
-    return pathNames
-
-#
-# =============================================================================
-#
-
-def make_sample_data(pathNames):
-    """
-    Generates sample data in the 'Raw' folder of a provided dictionary of file
-    locations.
-
-    Parameters
-    ----------
-    pathNames : dict-str
-        Dictionary of file locations.
-
-    Raises
-    ------
-    Exception
-        An exception is raised if the provided 'pathNames' dictionary doesn't
-        contain a 'Raw' path key.
-    Exception
-        An exception is raised if the sample data cannot be loaded.
-
-    Returns
-    -------
-    None.
-
-    """
-    
-    # Check that a 'raw' folder exists
-    if 'Raw' not in pathNames:
-        raise Exception('Raw path not detected in pathNames.')
-    
-    # Load the sample data
-    try:
-        print(importlib_resources.files("EMGFlow").joinpath(os.path.join("data", "sample_data_01.csv")))
-        sample_data_01 = pd.read_csv(importlib_resources.files("EMGFlow").joinpath(os.path.join("data", "sample_data_01.csv")))
-        sample_data_02 = pd.read_csv(importlib_resources.files("EMGFlow").joinpath(os.path.join("data", "sample_data_02.csv")))
-    except:
-        raise Exception('Failed to load EMGFlow sample data.')
-    
-    # Write the sample data
-    os.makedirs(os.path.join(pathNames['Raw'], '01'), exist_ok=True)
-    os.makedirs(os.path.join(pathNames['Raw'], '02'), exist_ok=True)
-    data_path_01 = os.path.join(pathNames['Raw'], '01', 'sample_data_01.csv')
-    data_path_02 = os.path.join(pathNames['Raw'], '02', 'sample_data_02.csv')
-    if not os.path.exists(data_path_01):
-        sample_data_01.to_csv(data_path_01, index=False)
-    if not os.path.exists(data_path_02):
-        sample_data_02.to_csv(data_path_02, index=False)
+    return pathDF
---
+++ EMGFlow-Package/tests/test_access_files.py
@@ -0,0 +1,77 @@
+import unittest
+import importlib
+import os
+import shutil
+
+# Load EMGFlow from local files
+filePath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'EMGFlow', '__init__.py'))
+spec = importlib.util.spec_from_file_location("EMGFlow", filePath)
+EMGFlow = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(EMGFlow)
+
+import pandas as pd
+import sys
+import numpy as np
+
+#
+# =============================================================================
+#
+
+class TestSimple(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+#
+# =============================================================================
+#
+    
+    def test_make_paths(self):
+        pathNames = EMGFlow.make_paths()
+        self.assertIsInstance(pathNames, dict)
+        self.assertTrue(os.path.isdir('Data'))
+    
+    def test_make_sample_data(self):
+        pathNames = EMGFlow.make_paths()
+        EMGFlow.make_sample_data(pathNames)
+        self.assertTrue(os.path.exists(os.path.join('Data', 'Raw', '01', 'sample_data_01.csv')))
+    
+    def test_read_file_type(self):
+        pathNames = EMGFlow.make_paths()
+        EMGFlow.make_sample_data(pathNames)
+        df = EMGFlow.read_file_type(os.path.join(pathNames['Raw'], '01', 'sample_data_01.csv'), 'csv')
+        self.assertIsInstance(df, pd.DataFrame)
+    
+    def test_map_files(self):
+        pathNames = EMGFlow.make_paths()
+        EMGFlow.make_sample_data(pathNames)
+        filedirs = EMGFlow.map_files(pathNames['Raw'])
+        self.assertIsInstance(filedirs, dict)
+    
+    def test_convert_map_files(self):
+        pathNames = EMGFlow.make_paths()
+        EMGFlow.make_sample_data(pathNames)
+        filedirs = EMGFlow.convert_map_files(pathNames['Raw'])
+        filedirs = EMGFlow.convert_map_files(filedirs)
+        self.assertIsInstance(filedirs, dict)
+    
+    def test_map_files_fuse(self):
+        pathNames = EMGFlow.make_paths()
+        EMGFlow.make_sample_data(pathNames)
+        pathDF = EMGFlow.map_files_fuse([pathNames, pathNames], ['test1', 'test2'])
+        self.assertIsInstance(pathDF, pd.DataFrame)
+    
+#
+# =============================================================================
+#
+
+    def tearDown(self):
+        if os.path.exists('./Data') == True:
+            shutil.rmtree('./Data')
+
+#
+# =============================================================================
+#
+
+if __name__ == '__main__':
+    unittest.main()
