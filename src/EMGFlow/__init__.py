45e1faf54ceb69ca65e180080094c1074e94ae04
WiIIson <69092314+WiIIson@users.noreply.github.com>
2025-07-31 17:42:55 -0400

    Removed handle_missing_data
    Removed handle_missing_data module, merged its functions with preprocess_signals

--- src/EMGFlow/__init__.py
+++ src/EMGFlow/__init__.py
@@ -1,6 +1,5 @@
 __version__ = "1.0.17"
 from .access_files import *
-from .preprocess_signals import *
 from .extract_features import *
-from .handle_missing_data import *
 from .plot_signals import *
+from .preprocess_signals import *
--- tests/test_preprocess_signals.py
+++ tests/test_preprocess_signals.py
@@ -114,6 +114,20 @@ class TestSimple(unittest.TestCase):
         EMGFlow.screen_artefact_signals(pathNames['Raw'], pathNames['Filled'], 2000)
         self.assertTrue(os.path.exists(os.path.join(pathNames['Filled'], '01', 'sample_data_01.csv')))
 
+    def test_apply_fill_missing(self):
+        pathNames = EMGFlow.make_paths()
+        filePath = os.path.join(pathNames['Raw'], '01', 'sample_data_01.csv')
+        Signal = EMGFlow.read_file_type(filePath, 'csv')
+        ASignal = EMGFlow.apply_screen_artefacts(Signal, 'EMG_zyg')
+        FSignal = EMGFlow.apply_fill_missing(ASignal, 'EMG_zyg')
+        self.assertIsInstance(FSignal, pd.DataFrame)
+    
+    def test_fill_missing_signals(self):
+        pathNames = EMGFlow.make_paths()
+        EMGFlow.screen_artefact_signals(pathNames['Raw'], pathNames['Filled'], 2000)
+        EMGFlow.fill_missing_signals(pathNames['Filled'], pathNames['Filled'], 2000)
+        self.assertTrue(os.path.exists(os.path.join(pathNames['Filled'], '01', 'sample_data_01.csv')))
+
 #
 # =============================================================================
 #
